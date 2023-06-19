3c1216bebdaa44cbb2b35a29982be5ca98979eb5
Maks Oganesyan <maxogan099@gmail.com>
2023-06-19 15:26:49 +0300

    Creating a Pygame window and handling input

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+
+class AlienInvasion:
+    """ Класс для управления ресурсами и поведением игры"""
+    def __init__(self) -> None:
+        """ Инициализирует игру и создаёт игровые ресурсы """
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1000, 700))
+        pygame.display.set_caption("Alien invasion")
+        
+    def run_game(self):
+        """ Запуск основного цикла игры """
+        while True:
+            #* Отслеживание событий клавиатуры и мыши 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+if __name__ == "__main__":
+    # Создание экземпляра и запуск игры
+    ai = AlienInvasion
+    ai.run_game()
