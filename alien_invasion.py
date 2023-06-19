c95d5c4a746d95289a67707dca4b9e8327b4991c
Maks Oganesyan <maxogan099@gmail.com>
2023-06-19 15:52:37 +0300

    Background Color Assignment.

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ class AlienInvasion:
         
         self.screen = pygame.display.set_mode((1000, 700))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230,230,230)
         
     def run_game(self):
         """ Запуск основного цикла игры """
