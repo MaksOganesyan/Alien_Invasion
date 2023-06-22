1e1c7205686dd98af4cb4cd47849e622cafa83e6
Maks Oganesyan <maxogan099@gmail.com>
2023-06-22 21:55:51 +0300

    Continuous movement

--- ship.py
+++ ship.py
@@ -8,7 +8,16 @@ class Ship():
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         
+        # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom =  self.screen_rect.midbottom
+        #? Флаг перемещения
+        self.moving_right = False
+        
+    def update(self):
+         """ Обновляет позицию корабля с учётом флага """
+         if self.moving_right:
+             self.rect.x += 1
+        
         
     def blitme(self):
         """ Рисует корабль в текущей  позиции  """
