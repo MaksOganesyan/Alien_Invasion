2916fca6afadfe5dfdeedf78fd741093f1cdd814
Maks Oganesyan <maxogan099@gmail.com>
2023-06-22 17:06:22 +0300

    refactoring

--- ship.py
+++ ship.py
@@ -5,9 +5,11 @@ class Ship():
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+       
         # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+       
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
