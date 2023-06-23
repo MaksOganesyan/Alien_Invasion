61f9a2428b8f487f7ff6435f8bc84956dbcd9d36
Maks Oganesyan <maxogan099@gmail.com>
2023-06-23 19:19:55 +0300

    Restriction of movements

--- ship.py
+++ ship.py
@@ -22,10 +22,10 @@ class Ship():
         
     def update(self):
         """ Обновляет позицию корабля с учётом флагов """
-        #? Обновляет атрибут x, не rect 
-        if self.moving_right:
+        #? Обновляет атрибут x обЪекта ship, не rect 
+        if self.moving_right and self.rect.right < self.screen_rect.right:
            self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
             
         #* Обновляет атрибут |rect| на основании self.x
